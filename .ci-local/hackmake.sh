bf35aa8f22e743173f0afd2ecb4c8e8a8fe995a1
Heesterman, Peter J <Peter.Heesterman@ukaea.uk>
2022-08-23 16:58:25 +0100

    Applied executable attribute to .sh scripts.

--- .ci-local/hackmake.sh
+++
@@ -1,4 +0,0 @@
-sed -i -e '/^include \$(TOP)\/ADApp\/commonLibraryMakefile/i ifeq (mingw, \$(findstring mingw, \$(T_A)))' /builds/DATAnet/adurl/.cache/adcore-R3-11/ADApp/pluginSrc/Makefile
-sed -i -e '/^include \$(TOP)\/ADApp\/commonLibraryMakefile/i \ \ USR_LDFLAGS += -Wl,-allow-multiple-definition' /builds/DATAnet/adurl/.cache/adcore-R3-11/ADApp/pluginSrc/Makefile
-sed -i -e '/^include \$(TOP)\/ADApp\/commonLibraryMakefile/i endif' /builds/DATAnet/adurl/.cache/adcore-R3-11/ADApp/pluginSrc/Makefile
-
